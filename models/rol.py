70facfbefde37ae7370641681792f3bd7d815bf3
DanielGarciaPluma <230187@utxicotepec.edu.mx>
2026-02-17 12:53:41 -0600

    Actualizacion de archivos
    Se actualizaron archivos de las carpetas

--- models/rol.py
+++ models/rol.py
@@ -1,13 +1,12 @@
-''' En esta clase permite generar el model para los tipos de rol'''
-from sqlalchemy import Column, Integer, String, Boolean, DateTime, Enum, Date
-from sqlalchemy.orm import relationship
+''''Esta clase permite generar el modelo para los tipos de roles'''
+from sqlalchemy import Column, Integer, String, Boolean,Column,DateTime
 from config.db import Base
 
 class Rol(Base):
-    '''Clase para especificar la tabla roles de usuario'''
-    _tablemname_="tbc_roles"
-
-    Id = Column(Integer, primary_key=True, index=True)
-    nombreRol = Column(String(15))
-    estado = Column(Boolean)
-    
+    '''Modelo para la tabla de roles'''
+    __tablename__ = "tbc_roles"
+    id = Column(Integer, primary_key=True, index=True)
+    nombre_rol = Column(String(50), unique=True, index=True, nullable=False)
+    estado = Column(Boolean, default=True)
+    fecha_registro= Column(DateTime)
+    fecha_modificacion= Column(DateTime)
